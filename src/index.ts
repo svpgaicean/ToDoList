eb241d8cd35a3d2c14223bea34d03b31419808c0
Patrik Gaicean <patrik.gaicean@softvision.ro>
2019-10-03 15:54:26 +0300

    Add database transactions for 'addUser' and 'usernameExists'

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-import { registerNewUser } from './registration';
+// import { registerNewUser } from './registration';
+import { Database } from './database';
+import { IUser } from './utils';
 
-registerNewUser();
+const dbBtn = document.querySelector('#db-btn') as HTMLFormElement;
+
+(function() {
+	// registerNewUser();
+	let db = new Database('database1');
+
+	let user: IUser = {
+		username: 'a',
+		password: 'ab',
+		email: 'agdf',
+		deleted: false
+	};
+
+	dbBtn.addEventListener('click', () => { db.addUser(user) });
+}());
--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,4 @@
-interface IOptions {
-	[option: string]: RegExp;
-};
-
-export const inputValidationRegex: IOptions = {
-	username: /^[a-zA-Z\d.]{6,}$/,
-	password: /^(?!.*?[\^])(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Z]+.{7,}$/,
-	confirmed: /^(?!.*?[\^])(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Z]+.{7,}$/,
-	email: /^.*$/
-};
-
-export class Authentication {
+class Authentication {
 	protected _username: string;
 	protected _password: string;
 
@@ -21,3 +10,27 @@ export class Authentication {
 	set username(value: string) { this._username = value; };
 	set password(value: string) { this._password = value; };
 }
+
+interface IUser {
+	username: string,
+	password: string,
+	email: string,
+	deleted: boolean
+}
+
+interface IOptions {
+	[option: string]: RegExp;
+};
+
+const inputValidationRegex: IOptions = {
+	username: /^[a-zA-Z\d.]{6,}$/,
+	password: /^(?!.*?[\^])(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Z]+.{7,}$/,
+	confirmed: /^(?!.*?[\^])(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Z]+.{7,}$/,
+	email: /^.*$/
+};
+
+export {
+	Authentication,
+	IUser,
+	inputValidationRegex
+};
